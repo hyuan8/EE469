61f677a5521d70f95846b871e8ccc228bb99e003
Hailey Yuan <hailey@mopdit.com>
2026-05-03 15:52:35 -0700

    updated instruction fetch and datapath

--- lab3/instruction_fetch.sv
+++ lab3/instruction_fetch.sv
@@ -1,11 +1,8 @@
-module instruction_fetch()
-
-	input logic [63:0] inputBits;
-	input logic [1:0] UncondBr, BrTaken;
-	input logic reset, clk;
-	
-	output logic [31:0] instruction;
-	output logic [63:0] unbranchedAddr;
+module instruction_fetch(
+	input logic UncondBr, BrTaken, // signals from control
+	input logic reset, clk,
+	output logic [31:0] instruction, // 32-bit instruction
+	output logic [63:0] unbranchedAddr); // PC + 4
 	
 	// Register for program counter (PC)
 	// puts current PC into instruction memory
@@ -13,19 +10,20 @@ module instruction_fetch()
 	program_counter pc (.in(newPC), .out(currentPC), .clk, .reset);
 	
 	// Instruction memory
-	instructmem instruction_memory (.address(inputBits), .instruction(instruction), .clk));
+	instructmem instruction_memory (.address(currentPC), .instruction(instruction), .clk);
 	
-	// Sign exenders for CondAddr19 and BrAddr26
-	sign_extender #(.N(19)) condAddr (.in(Instruction[23:5]), .out(condAddr));
-	sign_extender #(.N(26)) BrAddr (.in(Instruction[25:0]), .out(uncondAddr));
+	// Sign exenders for condAddr19 and BrAddr26
+	logic [63:0] condOffset, brOffset;
+	sign_extender #(.length(19)) extender1 (.in(instruction[23:5]), .out(condOffset));
+	sign_extender #(.length(26)) extender2 (.in(instruction[25:0]), .out(brOffset));
 	
-	// Mux taking UncondBr
+	// UncondBr Mux
 	logic [63:0] brAddr;
-	mux2_1_64bits (.out(brAddr), .A(condAddr), .B(uncondAddr), .sel(UncondBr));
+	mux2_1_Nbits #(.length(64)) UncondBrMux (.out(brAddr), .A(condOffset), .B(brOffset), .sel(UncondBr));
 	
 	// Shifter that multiplies by 4
 	logic [63:0] shiftedAddr;
-	shifter shift2 (.value(brAddr), .direction(6'd2), .distance(2'b10), .result(shiftedAddr));
+	shifter shift2 (.value(brAddr), .direction(1'b0), .distance(6'd2), .result(shiftedAddr));
 	
 	// Adder for branching
 	logic [63:0] branchedAddr;
@@ -34,9 +32,8 @@ module instruction_fetch()
 	// Adder for going down (not branching)
 	full_adder_64bits incAddr (.result(unbranchedAddr), .A(currentPC), .B(64'd4), .Cin(1'b0), .Cout());
 	
-	// Mux to determine whether or not to branch
-	mux2_1_Nbits #(.length(64)) choosePC (.out(brAddr), .A(condAddr), .B(uncondAddr), .sel(BrTaken));
+	// BrTaken Mux
+	mux2_1_Nbits #(.length(64)) BrTakenMux (.out(newPC), .A(unbranchedAddr), .B(branchedAddr), .sel(BrTaken));
 
 endmodule
-	
 	
