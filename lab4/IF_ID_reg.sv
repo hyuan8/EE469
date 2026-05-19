edecbbc556cb9c8c7ec86150f964253ad386d542
nadia chowdhury <nadiac6@uw.edu>
2026-05-18 23:01:34 -0700

    minor changes + rewiring cpu in progress

--- lab4/IF_ID_reg.sv
+++ lab4/IF_ID_reg.sv
@@ -6,7 +6,7 @@ module IF_ID_reg (
 	output logic [63:0] PC_out
 );
 
-	generateDFFs #(.N(32)) instruction_DFFs (.d(instruction), .q(instruction_out), .clk(clk), .reset(reset));
-	generateDFFs #(.N(64)) PC_DFFs (.d(PC_in), .q(PC_out), .clk(clk), .reset(reset));
+	DFFs #(.N(32)) instruction_DFFs (.d(instruction), .q(instruction_out), .clk(clk), .reset(reset));
+	DFFs #(.N(64)) PC_DFFs (.d(PC), .q(PC_out), .clk(clk), .reset(reset));
 	
 endmodule
--- lab4/cpu.sv
+++ lab4/cpu.sv
@@ -1,6 +1,7 @@
 `timescale 1ns/10ps
 
-/* This module builds the CPU.
+/* OLD CPU
+// This module builds the CPU.
 
 	INPUTS:
 	- clk: 		clock
@@ -32,50 +33,116 @@
 	- ALUzero: 		unregistered zero flag for CBZ instruction (BrTaken) 
 */
 
-module cpu (
-	input logic clk, reset
-);
+//module cpu (
+//	input logic clk, reset
+//);
+//
+//	logic [31:0] instruction;
+//	logic [63:0] unbranchedAddr;
+//	
+//	logic [2:0] ALUOp;
+//	logic ALUSrc, MemToReg, Reg2Loc, RegWrite, MemWrite, MemRead;
+//	
+//	logic negative, zero, overflow, carry_out;
+//	logic	UncondBr, BrTaken, SetFlags;
+//	
+//	logic BrLink, BrReg;
+//	logic [63:0] BrRegAddr;
+//	
+//	logic Imm12;
+//	logic [63:0] Db;
+//
+//	// Registered flags (using DFF w/ enable) - only update when SetFlags=1
+//	logic neg_reg, zero_reg, ov_reg, co_reg;
+//
+//	flag_register FR (.clk(clk), .reset(reset), .enable(SetFlags), 
+//		.negative(negative), .zero(zero), .overflow(overflow), .carry_out(carry_out), 
+//		.negative_out(neg_reg), .zero_out(zero_reg), .overflow_out(overflow_out), .carry_out_out(co_reg));
+//	
+//	// Instantiates instruction fetch
+//	instruction_fetch IF (.UncondBr(UncondBr), .BrTaken(BrTaken), .BrReg(BrReg), .BrRegAddr(BrRegAddr),
+//		.reset(reset), .clk(clk), .instruction(instruction), .unbranchedAddr(unbranchedAddr));
+//		
+//	// Instantiates control unit
+//	control CTL (.instruction(instruction), .negative(neg_reg), .zero(zero_reg), .overflow(ov_reg),
+//		.carry_out(co_reg), .ALUOp(ALUOp), .Reg2Loc(Reg2Loc), .ALUSrc(ALUSrc), .MemToReg(MemToReg),
+//		.RegWrite(RegWrite), .MemWrite(MemWrite), .MemRead(MemRead), .BrTaken(BrTaken), .Imm12(Imm12),
+//		.UncondBr(UncondBr), .SetFlags(SetFlags), .BrLink(BrLink), .BrReg(BrReg), .ALUzero(zero)) ;
+//		
+//	// Instantiates main datapath
+//	datapath DP (.instruction(instruction), .ALUOp(ALUOp), .ALUSrc(ALUSrc), .Mem2Reg(MemToReg), .BrLink(BrLink),
+//		.unbranchAddr(unbranchedAddr), .BrRegAddr(BrRegAddr), .Imm12(Imm12), .Db(Db),
+//		.Reg2Loc(Reg2Loc), .RegWrite(RegWrite), .MemWrite(MemWrite), .MemRead(MemRead), .clk(clk), .reset(reset), 
+//		.XferSize(4'd8), .negative(negative), .zero(zero), .overflow(overflow), .carry_out(carry_out));
+//	
+//endmodule
 
-	logic [31:0] instruction;
-	logic [63:0] unbranchedAddr;
+module cpu (input logic clock, reset);
+	logic [31:0] instruction_IF;
+	logic [63:0] currentPC, PC_4_IF, newPC;
 	
-	logic [2:0] ALUOp;
-	logic ALUSrc, MemToReg, Reg2Loc, RegWrite, MemWrite, MemRead;
+	//instruction fetch
+	//only is a register for the pc, does not do the addition, need full adder for this
+	program_counter PC (.in(newPC), .out(currentPC), .clk(clk), .reset(reset));
 	
-	logic negative, zero, overflow, carry_out;
-	logic	UncondBr, BrTaken, SetFlags;
+	full_adder_64bits PC_4 (.S(PC_4_IF), .A(currentPC), .B(64'd4), .Cin(1'b0), .Cout());
 	
-	logic BrLink, BrReg;
-	logic [63:0] BrRegAddr;
+	instructmem instruction_memory (.address(currentPC), .instruction(instruction_IF), .clk);
+ 
 	
-	logic Imm12;
-	logic [63:0] Db;
-
-	// Registered flags (using DFF w/ enable) - only update when SetFlags=1
+	//if/id reg
+	logic [31:0] instruction_ID;
+	logic [63:0] PC_4_ID;
+	IF_ID_reg IF_ID (.clk(clk), .reset(reset), .instruction(instruction_IF), .PC(PC_4_IF), .instruction_out(instruction_ID), .PC_out(PC_4_ID))
+	
+	
+	//instruction decode - moved from datapath
+	logic [2:0] ALUOp_ID;
+	logic ALUSrc_ID, MemToReg_ID, Reg2Loc_ID, RegWrite_ID, MemWrite_ID, MemRead_ID;
+	logic	UncondBr_ID, BrTaken_ID, SetFlags_ID;
+	logic BrLink_ID, BrReg_ID, Imm12_ID;
+	
+	logic [4:0] Rd_ID, Rm_ID, Rn_ID;
+	logic [63:0] Da_ID, Db_ID;
+	logic [4:0] Ab_ID;
+	
+	//write back signals
+	logic [4:0] Rd_WB;
+	logic [63:0] Dw_ID;
+	logic RegWrite_WB;
+	
+	//saved flags
 	logic neg_reg, zero_reg, ov_reg, co_reg;
 
-	flag_register FR (.clk(clk), .reset(reset), .enable(SetFlags), 
-		.negative(negative), .zero(zero), .overflow(overflow), .carry_out(carry_out), 
-		.negative_out(neg_reg), .zero_out(zero_reg), .overflow_out(overflow_out), .carry_out_out(co_reg));
 	
-	// Instantiates instruction fetch
-	instruction_fetch IF (.UncondBr(UncondBr), .BrTaken(BrTaken), .BrReg(BrReg), .BrRegAddr(BrRegAddr),
-		.reset(reset), .clk(clk), .instruction(instruction), .unbranchedAddr(unbranchedAddr));
+	assign Rd_ID = instruction_ID[4:0];
+	assign Rm_ID = instruction_ID[20:16];
+	assign Rn_ID = instruction_ID[9:5];
 		
-	// Instantiates control unit
-	control CTL (.instruction(instruction), .negative(neg_reg), .zero(zero_reg), .overflow(ov_reg),
-		.carry_out(co_reg), .ALUOp(ALUOp), .Reg2Loc(Reg2Loc), .ALUSrc(ALUSrc), .MemToReg(MemToReg),
-		.RegWrite(RegWrite), .MemWrite(MemWrite), .MemRead(MemRead), .BrTaken(BrTaken), .Imm12(Imm12),
-		.UncondBr(UncondBr), .SetFlags(SetFlags), .BrLink(BrLink), .BrReg(BrReg), .ALUzero(zero)) ;
-		
-	// Instantiates main datapath
-	datapath DP (.instruction(instruction), .ALUOp(ALUOp), .ALUSrc(ALUSrc), .Mem2Reg(MemToReg), .BrLink(BrLink),
-		.unbranchAddr(unbranchedAddr), .BrRegAddr(BrRegAddr), .Imm12(Imm12), .Db(Db),
-		.Reg2Loc(Reg2Loc), .RegWrite(RegWrite), .MemWrite(MemWrite), .MemRead(MemRead), .clk(clk), .reset(reset), 
-		.XferSize(4'd8), .negative(negative), .zero(zero), .overflow(overflow), .carry_out(carry_out));
+	control CTL (.instruction(instruction_ID), .negative(neg_reg), .zero(zero_reg), .overflow(ov_reg),
+		.carry_out(co_reg), .ALUOp(ALUOp_ID), .Reg2Loc(Reg2Loc_ID), .ALUSrc(ALUSrc_ID), .MemToReg(MemToReg_ID),
+		.RegWrite(RegWrite_ID), .MemWrite(MemWrite_ID), .MemRead(MemRead_ID), .BrTaken(BrTaken_ID), .Imm12(Imm12_ID),
+		.UncondBr(UncondBr_ID), .SetFlags(SetFlags_ID), .BrLink(BrLink_ID), .BrReg(BrReg_ID), .ALUzero(zero_EX)) ; //raw zero for ALU comes from ex stage
 	
-endmodule
 
+	mux2_1_Nbits #(.length(5)) Reg2LocMux (.out(Ab_ID), .A(Rd_ID), .B(Rm_ID), .sel(Reg2Loc_ID));
+	
+	sign_extender #(.length(9)) extender9 (.in(instruction[20:12]), .out(imm9_ID));
+	zero_extender #(.length(12)) extender12 (.in(instruction[21:10]), .out(imm12_ID));
+	
+	regfile register (.ReadData1(Da_ID), .ReadData2(Db_ID), .WriteData(Dw_WB), 
+				.ReadRegister1(Rn_ID), .ReadRegister2(Ab_ID), .WriteRegister(Aw_WB), .RegWrite(RegWrite_WB), .clk(clk));
+	
+	//id/ex reg
+	
+	ID_EX_reg ID_EX (
+	
+	
+	
+	
+	
+);
+	
 
 // Testbench for CPU -- runs clock, instructions read in instructionmem
 module cpu_testbench();
