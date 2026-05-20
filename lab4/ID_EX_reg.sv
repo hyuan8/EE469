2cbfd5f93313aa78fc5d88addcdfb88997b8d9e3
nadia chowdhury <nadiac6@uw.edu>
2026-05-20 12:44:47 -0700

    added blt, b, and br support

--- lab4/ID_EX_reg.sv
+++ lab4/ID_EX_reg.sv
@@ -1,19 +1,23 @@
 module ID_EX_reg (
 	input logic clk, reset,
-	input logic RegWrite, MemWrite, MemRead, MemToReg, 
+	input logic RegWrite, MemWrite, MemRead, MemToReg, UncondBr, BrTaken, 
 	input logic ALUSrc, SetFlags, BrLink, Imm12, BrReg,
 	input logic [2:0] ALUOp,
 	input logic [63:0] Da, Db,
 	input logic [4:0] Rn, Rm, Rd,
 	input logic [63:0] imm9, imm12,
 	input logic [63:0] PC_plus4,
+	input logic [63:0] condOffset, brOffset,
 	output logic RegWrite_out, MemWrite_out, MemRead_out, MemToReg_out,
 	output logic ALUSrc_out, SetFlags_out, BrLink_out, Imm12_out, BrReg_out,
 	output logic [2:0] ALUOp_out,
 	output logic [63:0] Da_out, Db_out,
 	output logic [4:0] Rn_out, Rm_out, Rd_out,
 	output logic [63:0] imm9_out, imm12_out,
-	output logic [63:0] PC_plus4_out
+	output logic [63:0] PC_plus4_out,
+	output logic UncondBr_out, BrTaken_out
+	output logic [63:0] condOffset_out, brOffset_out
+	
 );
 
 	D_FF RegWrite_DFF (.d(RegWrite), .q(RegWrite_out), .clk(clk), .reset(reset));
@@ -25,6 +29,11 @@ module ID_EX_reg (
 	D_FF BrLink_DFF 	(.d(BrLink), 	.q(BrLink_out), 	.clk(clk), .reset(reset));
 	D_FF Imm12_DFF 	(.d(Imm12), 	.q(Imm12_out), 	.clk(clk), .reset(reset));
 	D_FF BrReg_DFF 	(.d(BrReg), 	.q(BrReg_out), 	.clk(clk), .reset(reset));
+	D_FF UncondBr_DFF (.d(UncondBr), .q(UncondBr_out), .clk(clk), .reset(reset));
+	D_FF BrTaken_DFF 	(.d(BrTaken), 	.q(BrTaken_out), 	.clk(clk), .reset(reset));
+
+
+	
 	
 	DFFs #(.N(3))	ALUOp_DFF	(.d(ALUOp), .q(ALUOp_out), .clk(clk), .reset(reset));
 	
@@ -37,6 +46,10 @@ module ID_EX_reg (
 	DFFs #(.N(64))	imm9_DFFs 	(.d(imm9), 	.q(imm9_out), 	.clk(clk), .reset(reset));
 	DFFs #(.N(64))	imm12_DFFs 	(.d(imm12), .q(imm12_out), .clk(clk), .reset(reset));
 	
-	DFFs #(.N(64)) PC_plus4_DFFs	(.d(PC_plus4), .q(PC_plus4_out), .clk(clk), .reset(reset));
+	DFFs #(.N(64)) PC_plus4_DFFs		(.d(PC_plus4), .q(PC_plus4_out), .clk(clk), .reset(reset));
+	DFFs #(.N(64)) cond_offset_DFFs	(.d(condOffset), .q(condOffset_out), .clk(clk), .reset(reset));
+	DFFs #(.N(64)) brOffset_DFFs		(.d(brOffset), .q(brOffset_out), .clk(clk), .reset(reset));
+
+
 	
 endmodule
