70a0f723deeb936ba25f394c00331dcae0da9edd
Hailey Yuan <hailey@mopdit.com>
2026-05-21 01:22:34 -0700

    fixed logic declaration errors

--- lab4/cpu.sv
+++ lab4/cpu.sv
@@ -88,7 +88,11 @@ module cpu (input logic clk, reset);
 	full_adder_64bits PC_4 (.S(PC_plus4_IF), .A(currentPC), .B(64'd4), .Cin(1'b0), .Cout());
 	
 	instructmem instruction_memory (.address(currentPC), .instruction(instruction_IF), .clk);
+	logic [4:0] Rd_MEM;
 	
+	logic BrTaken_MEM, BrReg_MEM;
+	logic [63:0] branchedAddr_MEM;
+	logic [63:0] BrRegAddr_MEM;
 	logic [63:0] immBranchPC; // PC from immediate branch
 	mux2_1_Nbits #(.length(64)) BrTakenMux (.out(immBranchPC), .A(branchedAddr_MEM), .B(PC_plus4_IF), .sel(BrTaken_MEM));
 	
@@ -99,6 +103,9 @@ module cpu (input logic clk, reset);
 	logic [31:0] instruction_ID;
 	logic [63:0] PC_plus4_ID;
 	IF_ID_reg IF_ID (.clk(clk), .reset(reset), .instruction(instruction_IF), .PC(PC_plus4_IF), .instruction_out(instruction_ID), .PC_out(PC_plus4_ID));
+	
+	logic negative_EX, zero_EX, overflow_EX, carry_out_EX;
+
 	
 	//instruction decode
 	logic [2:0] ALUOp_ID;
@@ -140,6 +147,7 @@ module cpu (input logic clk, reset);
 	sign_extender #(.length(19)) extender1 (.in(instruction_ID[23:5]), .out(condOffset_ID));
 	sign_extender #(.length(26)) extender2 (.in(instruction_ID[25:0]), .out(brOffset_ID));
 	
+	logic [63:0] Dw_WB;
 	regfile register (.ReadData1(Da_ID), .ReadData2(Db_ID), .WriteData(Dw_WB), 
 				.ReadRegister1(Rn_ID), .ReadRegister2(Ab_ID), .WriteRegister(Rd_WB), .RegWrite(RegWrite_WB), .clk(clk));
 	
@@ -150,9 +158,12 @@ module cpu (input logic clk, reset);
 	logic [63:0] Da_EX, Db_EX;
 	logic [4:0] Rn_EX, Rm_EX, Rd_EX;
 	logic [63:0] imm9_EX, imm12_EX, PC_plus4_EX;
-	logic [63:0] Dw_WB;
+	
 	logic [4:0] Aw_WB;
 
+	logic UncondBr_EX, BrTaken_EX; 
+	logic [63:0] condOffset_EX, brOffset_EX;
+	
 	
 	ID_EX_reg ID_EX (.clk(clk), .reset(reset), .RegWrite(RegWrite_ID), .MemWrite(MemWrite_ID), 
 	.MemRead(MemRead_ID), .MemToReg(MemToReg_ID), .UncondBr(UncondBr_ID), .BrTaken(BrTaken_ID), .ALUSrc(ALUSrc_ID), 
@@ -167,8 +178,7 @@ module cpu (input logic clk, reset);
 	.brOffset_out(brOffset_EX));
 	
 	//execute stage
-	logic UncondBr_EX, BrTaken_EX; 
-	logic [63:0] condOffset_EX, brOffset_EX;
+	
 	logic [63:0] brAddr_EX, shiftedAddr_EX, branchedAddr_EX;
 	logic [4:0] WriteReg_EX;
 
@@ -180,11 +190,13 @@ module cpu (input logic clk, reset);
 	logic [63:0] final_imm_EX;
 	mux2_1_Nbits #(.length(64)) FinalImmMux (.out(final_imm_EX), .A(imm9_EX), .B(imm12_EX), .sel(Imm12_EX));
 	
+	logic RegWrite_MEM;
 	logic [1:0] ForwardA, ForwardB;
 	forwarding_unit FU (.Rn_EX(Rn_EX), .Ab_EX(Rm_EX), .Rd_MEM(Rd_MEM), .Rd_WB(Rd_WB), .RegWrite_MEM(RegWrite_MEM),
 		.RegWrite_WB(RegWrite_WB), .ForwardA(ForwardA), .ForwardB(ForwardB));
 	
 	logic [63:0] ForwardA_out, ForwardB_out;
+	logic [63:0] ALUOut_MEM;
 	mux4_1_Nbits #(.length(64)) ForwardAMux (.out(ForwardA_out), .A(Da_EX), .B(ALUOut_MEM), .C(Dw_WB), .D(64'd0), .sel(ForwardA));
 	mux4_1_Nbits #(.length(64)) ForwardBMux (.out(ForwardB_out), .A(Db_EX), .B(ALUOut_MEM), .C(Dw_WB), .D(64'd0), .sel(ForwardB));
 	
@@ -193,21 +205,19 @@ module cpu (input logic clk, reset);
 	mux2_1_Nbits #(.length(64)) ALUSrcMux (.out(ALUB_EX), .A(ForwardB_out), .B(final_imm_EX), .sel(ALUSrc_EX));
 	
 	logic [63:0] ALUOut_EX;
-	logic negative_EX, zero_EX, overflow_EX, carry_out_EX;
 	alu ALU (.A(ForwardA_out), .B(ALUB_EX), .cntrl(ALUOp_EX), .result(ALUOut_EX), .negative(negative_EX), .zero(zero_EX), .overflow(overflow_EX), .carry_out(carry_out_EX));
 	
 	//BrLink mux to choose which register to write to if BrLink is true
 	mux2_1_Nbits #(.length(5)) BrLinkRegMux (.out(WriteReg_EX), .A(Rd_EX), .B(5'd30), .sel(BrLink_EX));
 	
 	//ex/mem reg
-	logic RegWrite_MEM, MemWrite_MEM, MemRead_MEM, MemToReg_MEM, BrLink_MEM, BrTaken_MEM, BrReg_MEM;
-	logic [63:0] ALUOut_MEM;
+	logic MemWrite_MEM, MemRead_MEM, MemToReg_MEM, BrLink_MEM;
+	
 	logic [63:0] Db_MEM;
-	logic [63:0] branchedAddr_MEM;
-	logic [63:0] BrRegAddr_MEM;
+	
 
 	logic SetFlags_MEM;
-	logic [4:0] Rd_MEM;
+	
 	logic negative_MEM, zero_MEM, overflow_MEM, carry_out_MEM;
 	
 	
@@ -263,7 +273,7 @@ module cpu_testbench();
 	
 	int i;
 	initial begin
-		reset = 1; @(posedge clk); @(posedge clk);
+		reset = 1; @(posedge clk); repeat(8);
 		reset = 0; @(posedge clk);
 		for (i = 0; i < 1000; i++) begin
 			@(posedge clk);
